057bddca96341704364b8816234ae176f6ae77f9
pbohea <pbohea@gmail.com>
2025-03-28 18:37:26 +0000

    refactored render statements

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,6 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new"
   end
 
   def index
@@ -15,9 +14,7 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index" 
-      end
+      format.html 
     end
   end
 
@@ -28,7 +25,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/show" 
   end
 
   def create
@@ -40,7 +36,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie was successfully created." 
     else
-      render template: "movies/new"
+      render template: "new"
     end
   end
 
@@ -51,7 +47,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/edit" 
   end
 
   def update
