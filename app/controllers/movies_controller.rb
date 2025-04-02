9a4c760a8a97290f165aae5a5a7ea992331becd4
pbohea <pbohea@gmail.com>
2025-04-02 13:25:40 +0000

    refactored update method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,9 +22,9 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
@@ -40,10 +40,9 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params.fetch(:id))
-    @movie.title = params.fetch("title")
-    @movie.description = params.fetch("description")
+    movie_attributes = params.require(:movie).permit(:title, :description)
 
-    if @movie.valid?
+    if @movie.update(movie_attributes)
       @movie.save
       redirect_to movie_url(@movie),  notice: "Movie updated successfully." 
     else
